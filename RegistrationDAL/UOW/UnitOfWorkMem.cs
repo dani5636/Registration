74a15b7e4456be28cb5785c0e4251f475287f7f6
Jeppe Ehmsen <jeppeehmsen@gmail.com>
2017-09-21 10:20:16 +0200

    wTF?!

--- RegistrationDAL/UOW/UnitOfWorkMem.cs
+++ RegistrationDAL/UOW/UnitOfWorkMem.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using RegistrationDAL.Context;
+using RegistrationDAL.Repositories;
+
 namespace RegistrationDAL.UOW
 {
     class UnitOfWorkMem : IUnitOfWork
